6ab8a46e2ed4c2d1c3abccaf3100af252a910a49
robinfan <robinfan@live.cn>
2016-02-01 21:44:54 +0800

    add sensor files

---
+++ srf05.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+Trig_Pin = 17
+Echo_Pin = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Trig_Pin, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(Echo_Pin, GPIO.IN)
+
+time.sleep(2)
+
+def checkdist():
+    GPIO.output(Trig_Pin, GPIO.HIGH)
+    time.sleep(0.000015)
+    GPIO.output(Trig_Pin, GPIO.LOW)
+    while not GPIO.input(Echo_Pin):
+        pass
+    t1 = time.time()
+    while GPIO.input(Echo_Pin):
+        pass
+    t2 = time.time()
+    return (t2-t1) *340/2
+
+try:
+    while True:
+        print 'Distance:%0.2f m' % checkdist()
+        time.sleep(3)
+except KeyboardInterrupt:
+    GPIO.cleanup()
